7b01f4861cba2ebe98e9201e7a510c6eac5094b0
Alikley <alikley933@gmail.com>
2025-02-02 00:02:20 +0330

    new

--- src/components/MovieSlider.tsx
+++ src/components/MovieSlider.tsx
@@ -1,6 +1,7 @@
 import useMovies from "@/hooks/useMovies";
 import { Image } from "@chakra-ui/react";
 import Carousel from "react-bootstrap/Carousel";
+import { CiCircleInfo } from "react-icons/ci";
 function MovieSlider() {
   const { data, isLoading, error } = useMovies();
 
@@ -12,17 +13,28 @@ function MovieSlider() {
     <div>
       <Carousel>
         {data.map((movie) => (
-          <Carousel.Item key={movie.id}>
-            <Image
-              className="d-block w-100 h-25"
-              src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-              alt={movie.original_title}
-            />
-            <Carousel.Caption>
-              <h5>{movie.original_title}</h5>
-              <p>{movie.overview}</p>
-            </Carousel.Caption>
-          </Carousel.Item>
+            <Carousel.Item key={movie.id}>
+              <Image
+                className="d-block w-100 h-25"
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                alt={movie.original_title}
+              />
+              <div
+                className="absolute inset-0 flex flex-col justify-leftr items-left text-left px-8 pt-80 bg-black
+              bg-opacity-50 text-[#ffffffac]"
+              >
+                <h5 className="text-18xl font-bold mb-4 space tracking-wide">
+                  {movie.original_title}
+                </h5>
+                <p className="text-xl max-w-2xl ">{movie.overview}</p>
+                <button className="mt-4 p-2 bg-[#9d9a96ac] font-bold rounded-lg duration-300 ease-in-out hover:bg-white hover:text-black flex flex-row-reverse items-center justify-around w-28">
+                  More Info
+                  <span style={{fontSize:"20px"}}>
+                    <CiCircleInfo />
+                  </span>
+                </button>
+              </div>
+            </Carousel.Item>
         ))}
       </Carousel>
     </div>
