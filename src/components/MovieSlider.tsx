77a19316995bf193e6286b1ca58ec4f57aff510e
Alikley <alikley933@gmail.com>
2025-02-02 00:12:52 +0330

    new

--- src/components/MovieSlider.tsx
+++ src/components/MovieSlider.tsx
@@ -20,13 +20,13 @@ function MovieSlider() {
                 alt={movie.original_title}
               />
               <div
-                className="absolute inset-0 flex flex-col justify-leftr items-left text-left px-8 pt-80 bg-black
-              bg-opacity-50 text-[#ffffffac]"
+                className="absolute inset-0 flex flex-col justify-leftr items-left text-left px-8 pt-100 bg-black
+              bg-opacity-50 text-[#ffffff]"
               >
-                <h5 className="text-18xl font-bold mb-4 space tracking-wide">
+                <h5 className="text-18xl font-bold mb-4 space tracking-wide" style={{fontSize:"60px"}}>
                   {movie.original_title}
                 </h5>
-                <p className="text-xl max-w-2xl ">{movie.overview}</p>
+                <p className="text-xl max-w-3xl ">{movie.overview}</p>
                 <button className="mt-4 p-2 bg-[#9d9a96ac] font-bold rounded-lg duration-300 ease-in-out hover:bg-white hover:text-black flex flex-row-reverse items-center justify-around w-28">
                   More Info
                   <span style={{fontSize:"20px"}}>
